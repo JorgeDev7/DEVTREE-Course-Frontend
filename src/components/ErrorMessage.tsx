005198644254e958591c9e38654c2e2bd47f10bf
JorgeDev <jorgehumbertocolindres@gmail.com>
2025-12-26 11:05:50 -0600

    feat: add custom error message component

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,12 @@
+import { type ReactNode } from "react";
+
+type ErrorMessageProps = {
+  children: ReactNode;
+};
+export default function ErrorMessage({ children }: ErrorMessageProps) {
+  return (
+    <p className="bg-red-50 text-red-600 p-3 uppercase text-sm font-bold text-center rounded">
+      {children}
+    </p>
+  );
+}
