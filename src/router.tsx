da0c28c3f3bfff206abfce94b0582ac8b46f39c5
JorgeDev <jorgehumbertocolindres@gmail.com>
2025-12-28 00:22:39 -0600

    feat: add protected layout and views

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AppLayout from "./layouts/app/AppLayout";
 import AuthLayout from "./layouts/auth/AuthLayout";
+import LinkTreeView from "./views/app/LinkTreeView";
+import ProfileView from "./views/app/ProfileView";
 import LoginView from "./views/auth/LoginView";
 import RegisterView from "./views/auth/RegisterView";
 
@@ -10,6 +13,11 @@ export default function Router() {
         <Route element={<AuthLayout />}>
           <Route path="/auth/login" element={<LoginView />} />
           <Route path="/auth/register" element={<RegisterView />} />
+        </Route>
+
+        <Route path="/admin" element={<AppLayout />}>
+          <Route index element={<LinkTreeView />} />
+          <Route path="profile" element={<ProfileView />} />
         </Route>
       </Routes>
     </BrowserRouter>
